5c6f8af68d869bf3154921af0435ca86557fcf09
Juan Santa Cruz <jhsantacruz@icloud.com>
2015-01-18 22:42:18 -0800

    Add controller for the User

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,30 @@
+class Api::UsersController < Api::BaseController
+  def index
+    respond_with :api, users
+  end
+
+  def show
+    respond_with :api, user
+  end
+
+  def create
+    respond_with :api, users.create(user_params)
+  end
+
+  def destroy
+    respond_with :api, user.destroy
+  end
+
+  private
+    def users
+      @users ||= User.all
+    end
+
+    def user
+      @user ||= User.find(params[:id])
+    end
+
+    def users_params
+      params.permit(:username, :email, :password_digest)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+	namespace :api do
+    resources :messages, only: [:index, :create, :destroy]
+    resources :chat_rooms
+  end
+
   get "*path", to: "application#index"
   root 'application#index'
 end
