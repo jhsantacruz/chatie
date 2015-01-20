121ac3397a8dd7c8b1844f144a3525746a1b0cd6
Juan Santa Cruz <jhsantacruz@icloud.com>
2015-01-19 22:31:08 -0800

    Forgot to add a route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	namespace :api do
     resources :messages, only: [:index, :create, :destroy]
     resources :chat_rooms
+    resources :users
   end
 
   get "*path", to: "application#index"
