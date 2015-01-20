181d0aea339d280037023c723f1144c29d80b021
Juan Santa Cruz <jhsantacruz@icloud.com>
2015-01-19 22:27:00 -0800

    Added name attribute to chat rooms

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119062041) do
+ActiveRecord::Schema.define(version: 20150120062535) do
 
   create_table "chat_rooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "messages", force: :cascade do |t|
